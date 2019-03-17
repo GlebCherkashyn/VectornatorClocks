3c863cb084294f10d9202988fdf95d2b30f69b79
GlebCherkashyn <gleb.cherkashyn@gmail.com>
2019-03-17 13:16:17 +0200

    Initial commit

---
+++ VectornatorClocks/Screens/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VectornatorClocks
+//
+//  Created by Gleb Cherkashyn on 17.03.2019.
+//  Copyright © 2019 Gleb Cherkashyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
